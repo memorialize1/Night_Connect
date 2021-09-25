73558aa380efedfb7f43e86b2cafa4c98f6b2acd
memorialize1 <tenkiyohousha@gmail.com>
2021-09-25 13:40:40 +0000

    手直し

--- app/controllers/admins/homes_controller.rb
+++ app/controllers/admins/homes_controller.rb
@@ -1,6 +1,16 @@
 class Admins::HomesController < ApplicationController
-  
+
+  before_action :signed_in?
+
   def top
   end
-  
+
+
+  private
+
+  def signed_in?
+    if admin_signed_in?
+      redirect_to admins_users_path
+    end
+  end
 end
--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,14 +1,24 @@
 class Admins::UsersController < ApplicationController
-  
+
   def index
     @users = User.page(params[:page]).reverse_order
   end
-  
+
   def show
+    @user = User.find(params[:id])
+    @genre = Genre.all
   end
-  
+
   def update
+    @user = User.find(params[:id])
+    if @user.user_status == true
+      @user.update(user_status: false)
+      redirect_back(fallback_location: root_path)
+    else
+      @user.update(user_status: true)
+      redirect_back(fallback_location: root_path)
+    end
   end
-  
-  
+
+
 end
